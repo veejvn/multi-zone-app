e0f12fe3cef6288a71825e09765629ce733de547
VeeJ <vesy17032003@gmail.com>
2025-06-30 19:25:32 +0700

    Fix link URL to point to the correct Blog Zone

--- main-app/src/app/page.tsx
+++ main-app/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>🏠 Main App (Zone chính)</h1>
-      <Link href="https://multi-zone-main-app.vercel.app/blog" prefetch={false}>
+      <Link href="https://multi-zone-blog-app.vercel.app/blog" prefetch={false}>
         ➡️ Đi đến Blog Zone
       </Link>
     </main>
