21ffeae704c81271a75450a4eacf658222d5de25
VeeJ <vesy17032003@gmail.com>
2025-07-01 10:54:56 +0700

    Remove commented-out rewrites configuration and add package.json for workspaces

--- main-app/next.config.ts
+++ main-app/next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/blog/:path*',
-        destination: 'https://multi-zone-blog-app.vercel.app/blog/:path*',
-        //destination: 'http://localhost:3001/blog/:path*', // Local development URL for the blog zone  
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/blog/:path*',
+  //       destination: 'https://multi-zone-blog-app.vercel.app/blog/:path*',
+  //       //destination: 'http://localhost:3001/blog/:path*', // Local development URL for the blog zone  
+  //     },
+  //   ];
+  // },
+  basePath: '',
 };
 
 export default nextConfig;
